b49c5643f8955947fbdd1f2803a2f7554aa5622d
nagata-ichiko <nagataichiko2249@gmail.com>
2023-03-08 22:47:20 +0900

    SideBarコンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.scss";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>discode</h1>
-      </header>
+      <Sidebar />
     </div>
   );
 }
